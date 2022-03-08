229b2c4709ec65f74723da2079b40c42fb9f2a1e
DawidMiroyan <dawid.miroyan@student.uantwerpen.be>
2022-03-08 22:10:24 +0100

    Experiments WIP cap_eh_sweep, mcs_sweep

--- Experiments/cap_eh_sweep/plot.py
+++ Experiments/cap_eh_sweep/plot.py
@@ -5,7 +5,7 @@ expPath = '/home/dawid/Documents/RP1/IEEE-802.11ah-ns-3/Experiments/cap_eh_sweep
 dataPath = expPath + "/data/"
 files = os.listdir(dataPath)
 
-enableOption = set()
+capEnableOptions = set()
 capSizeOptions = set()
 ehOptions = set()
 
@@ -15,15 +15,15 @@ data = dict()
 for file in files:
 
     options = file.rstrip(".txt").split(sep="_")
-    eopt = options[0]
+    ceopt = int(options[0])
     csopt = int(options[1])
     ehopt = float(options[2])
 
-    enableOption.add(eopt)
+    capEnableOptions.add(ceopt)
     capSizeOptions.add(csopt)
     ehOptions.add(ehopt)
 
-    key = (eopt, csopt, ehopt)
+    key = (ceopt, csopt, ehopt)
 
     x = []
     y = []
@@ -39,27 +39,53 @@ for file in files:
     data[key] = {'x': x, 'y': y}
 
 
-def plotToggleCap():
-    for eopt in enableOption:
-        legend = []
-        for key in sorted(data):
-            if key[0]==eopt:
-                if key[1] in [3, 6, 10]:
-                    if key[2] in [0.001, 0.005]:
-                        plt.plot([x/1000 for x in data[key]['x']], data[key]['y'])
+def plotVaryEH():
+    for ceopt in capEnableOptions:
+        for capSize in capSizeOptions:
+            legend = []
+            for key in sorted(data):
+                if key[0]==ceopt and key[1]==capSize:
+                    plt.plot([x/1000 for x in data[key]['x']], data[key]['y'])
 
-                        legend.append( "size: " + str(key[1]) + ", eh: " + str(key[2]))
+                    legend.append( "size: " + str(key[1]) + ", eh: " + str(key[2]))
 
-        plt.axhline(y=1.8, label='V_{th low}', color='red', linestyle='dotted')
-        plt.axhline(y=3, label='V_{th high}', color='green', linestyle='dotted')
-        plt.ylim(bottom=1.5, top=3.5)
-
-
-        plt.legend(legend)
-        plt.xlabel('Time in seconds')
-        plt.ylabel('Capacitor voltage')
-
-        plt.savefig(expPath+"/remainingVoltage_" + eopt + ".png")
-        plt.clf()
+            plt.axhline(y=1.8, label='V_{th low}', color='red', linestyle='dotted')
+            plt.axhline(y=3, label='V_{th high}', color='green', linestyle='dotted')
+            plt.ylim(bottom=1.5, top=3.5)
 
 
+            plt.legend(legend)
+            plt.xlabel('Time in seconds')
+            plt.ylabel('Capacitor voltage')
+
+            plt.savefig(expPath+"/VariedEH_" + "payloadSize" + str(popt) + "_" + "capSize" + str(capSize) + ".png")
+            plt.clf()
+
+def plotVaryPayloadSize():
+    for eh in ehOptions:
+        for capSize in capSizeOptions:
+            legend = []
+            for key in sorted(data):
+                if key[2]==eh and key[1]==capSize:
+                    plt.plot([x/1000 for x in data[key]['x']], data[key]['y'])
+
+                    legend.append( "Payload size: " + str(key[0]))
+
+            plt.axhline(y=1.8, label='V_{th low}', color='red', linestyle='dotted')
+            plt.axhline(y=3, label='V_{th high}', color='green', linestyle='dotted')
+            plt.ylim(bottom=1.5, top=3.5)
+
+
+            plt.legend(legend)
+            plt.xlabel('Time in seconds')
+            plt.ylabel('Capacitor voltage')
+
+            plt.savefig(expPath+"/VariedPayload_" + "capSize" + str(capSize) + "_" + "eh" + str(eh) + ".png")
+            plt.clf()
+
+def plotVaryCapSize():
+    pass
+
+plotVaryEH()
+plotVaryPayloadSize()
+plotVaryCapSize()
